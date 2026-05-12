d4544f85337a5684bfde3d2950c7a18fe6db4ba2
shivasaiade09 <shivasaiade@gmail.com>
2026-05-12 10:01:57 +0530

    Add car data structure with multiple car entries

---
+++ car.js
@@ -0,0 +1,26 @@
+export const cars = [
+    {
+        id: 1,
+        name: "Phantom GT",
+        price: 150000,
+        category: "Exotic",
+        image: "https://images.unsplash.com/photo-1544636331-e26879cd4d9b?q=80&w=1000",
+        topSpeed: "210 mph"
+    },
+    {
+        id: 2,
+        name: "Tesla Roadster",
+        price: 200000,
+        category: "Electric",
+        image: "https://images.unsplash.com/photo-1560958089-b8a1929cea89?q=80&w=1000",
+        topSpeed: "250 mph"
+    },
+    {
+        id: 3,
+        name: "S-Class Luxury",
+        price: 110000,
+        category: "Luxury",
+        image: "https://images.unsplash.com/photo-1553440569-bcc63803a83d?q=80&w=1000",
+        topSpeed: "155 mph"
+    }
+];
